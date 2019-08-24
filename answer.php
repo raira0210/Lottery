85cd72cf3de563bc8d870c59772e3ccff18ad740
nextcomkk <kobayashi@nextcom.co.jp>
2019-08-24 17:50:06 +0900

    v1.3

--- answer.php
+++ answer.php
@@ -14,7 +14,7 @@ if(empty($_GET["file"]) || empty($_GET["ans"])){
 <title>簡易スピードくじ(仮作成)</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="robots" content="noindex" />
-<link rel="stylesheet" href="http://www.testhp.shop/css/style.css"><!--左のパスでCSSを指定してください、相対パスではなく絶対パスでお願いします。-->
+<link rel="stylesheet" href="https://nxlottery.azurewebsites.net/css/style.css"><!--左のパスでCSSを指定してください、相対パスではなく絶対パスでお願いします。-->
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
